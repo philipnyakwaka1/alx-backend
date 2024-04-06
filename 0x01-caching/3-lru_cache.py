38a133fa0b8e8e505f9ce8dd8a9c317cdaa4998e
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-06 22:30:45 +0300

    lru cache using doubly linked list

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -39,41 +39,41 @@ class LRUCache(BaseCaching):
 
     def put(self, key: Any, item: Any) -> None:
         """Adds an item in the cache"""
-        if key in self.hash:
-            node = self.hash[key]
-            node.val = item
-            self.shift_node(node)
-        else:
-            node = DoublyLinkedList(key, item)
-            if self.head is None and self.tail is None:
-                self.head = node
-                if self.tail is None:
-                    self.tail = self.head
-                self.hash[key] = node
-                self.cache_data[key] = node.val
-                self.length += 1
+        if key and item:
+            if key in self.hash:
+                node = self.hash[key]
+                node.val = item
+                self.shift_node(node)
             else:
-                self.hash[key] = node
-                self.cache_data[key] = node.val
-                self.tail.next = node
-                node.prev = self.tail
-                self.tail = node
-                self.length += 1
+                node = DoublyLinkedList(key, item)
+                if self.head is None and self.tail is None:
+                    self.head = node
+                    if self.tail is None:
+                        self.tail = self.head
+                    self.hash[key] = node
+                    self.cache_data[key] = node.val
+                    self.length += 1
+                else:
+                    self.hash[key] = node
+                    self.cache_data[key] = node.val
+                    self.tail.next = node
+                    node.prev = self.tail
+                    self.tail = node
+                    self.length += 1
 
-            if self.length > self.MAX_ITEMS:
-                del self.cache_data[self.head.key]
-                print(f'DISCARD: {self.head.key}')
-                del self.hash[self.head.key]
-                self.head = self.head.next
-                self.head.prev = None
-                self.length -= 1
+                if self.length > self.MAX_ITEMS:
+                    del self.cache_data[self.head.key]
+                    print(f'DISCARD: {self.head.key}')
+                    del self.hash[self.head.key]
+                    self.head = self.head.next
+                    self.head.prev = None
+                    self.length -= 1
 
     def get(self, key: Any) -> Union[None, Any]:
         """Get item by key"""
         val = self.hash.get(key)
         if val is None:
             return None
-        if val is not None:
-            node = self.hash[key]
-            self.shift_node(node)
-        return val.val
+        node = self.hash[key]
+        self.shift_node(node)
+        return self.cache_data[key]
