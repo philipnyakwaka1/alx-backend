7e6dabd216112aae7e30594b3aaaf43aa2557423
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-09 20:38:07 +0300

    pycodestyle

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for home page"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 app = Flask(__name__)
 
 
@@ -25,7 +25,10 @@ def get_locale():
 @app.route('/')
 def home_page() -> str:
     """renders index.html"""
-    return render_template('0-index.html')
+    return render_template('3-index.html',
+                           home_title=_("Welcome to Holberton"),
+                           home_header=_("Hello world!"))
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
