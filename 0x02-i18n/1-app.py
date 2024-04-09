cf0d8ffd41eccf93f19e8132f09d64c0ac862a78
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-09 19:30:17 +0300

    babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 """Module for home page"""
 from flask import Flask, render_template
+from flask_babel import Babel
 app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
