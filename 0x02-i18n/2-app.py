766613ee8b10a1b653342054607b5720f7f42035
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-04-09 19:52:13 +0300

    babel

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Module for home page"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config:
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['en', 'fr'])
+
+
+@app.route('/')
+def home_page() -> str:
+    """renders index.html"""
+    return render_template('0-index.html')
